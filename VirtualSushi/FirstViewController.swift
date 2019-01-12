a4297e4c7c3d35fd3b35fe4484eca895455b5ff5
Jordan Reed <2756092+chanomie@users.noreply.github.com>
2019-01-12 15:00:41 -0800

    Initial Commit

---
+++ VirtualSushi/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  VirtualSushi
+//
+//  Created by Jordan Reed on 1/12/19.
+//  Copyright © 2019 Chaos Corporation. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
